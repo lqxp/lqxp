29586f2b271188c4c103a951407a13eb75f0979e
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-04-28 15:55:07 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
-git pull;
-cargo build -r;
-cd web;
-bun install;
-bun vite build;
-cd ..;
-pm2 restart qxp-app;
+set -e
+
+git pull --recurse-submodules
+git submodule update --init --recursive
+
+cargo build -r
+
+cd web
+bun install
+bun vite build
+cd ..
+
+pm2 restart qxp-app
