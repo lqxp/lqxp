68cd1ed79cf88870a79d0a7c3c92be1a1abfef75
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-07-25 19:54:40 +0200

    pm2 file

---
+++ pm2.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'qxp-app',
+      port: 4560,
+      exec_mode: 'fork',
+      instances: 1,
+      script: 'cargo build -r ; ./target/release/qxprotocol',
+      error_file: require('path').join(require('os').homedir(), '.pm2/logs/qxchat-error.log'),
+      out_file: require('path').join(require('os').homedir(), '.pm2/logs/qxchat-out.log'),
+      merge_logs: true,
+      max_memory_restart: '512M',
+      watch: false,
+      autorestart: true,
+    },
+  ],
+}
